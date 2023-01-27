f48ac9f6442bedeab556fbcbdf7601c326ea4a2c
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-27 19:15:39 +0100

    filled NewContact page that return to us the lead data

--- resources/views/emails/new-contact-mail.blade.php
+++ resources/views/emails/new-contact-mail.blade.php
@@ -1,3 +1,8 @@
 <h1>Ciao, hai un nuovo messaggio</h1>
 
-<p>Sei stato contattato da:</p>
+<p>
+    Sei stato contattato da {{ $lead->name }}
+    Nome: {{ $lead->name }} <br>
+    Email: {{ $lead->email }} <br>
+    Messaggio: {{ $lead->message }} <br>
+</p>
